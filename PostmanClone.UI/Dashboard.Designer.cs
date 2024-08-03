b5fd52ecce6e57b78025912df943edec6aa52c94
Giovany Gonçalves <giovanycumino@outlook.com>
2024-08-03 18:22:33 -0300

    Inicial Desing of UI
    Based on Info that I get on the internet and desing that a an confortable with.

--- PostmanClone.UI/Dashboard.Designer.cs
+++ PostmanClone.UI/Dashboard.Designer.cs
@@ -28,20 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
+            HttpRequestPanel = new Panel();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label1 = new Label();
+            BodyRequestTextBox = new TextBox();
+            HttpHeadersRequestTextBox = new TextBox();
+            StartLineRequestTextBox = new TextBox();
+            HttpResponsePanel = new Panel();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label2 = new Label();
+            BodyResponseTextBox = new TextBox();
+            HttpHeadersResponseTextBox = new TextBox();
+            StartLineResponseTextBox = new TextBox();
+            MainPanel = new Panel();
+            StatusSystemToolStrip = new ToolStrip();
+            StatusSystemPanel = new Panel();
+            SystemControlPanel = new Panel();
+            SystemControlMenuStrip = new MenuStrip();
+            RequestControlPanel = new Panel();
+            StartRequestButton = new Button();
+            UrlRequestTextBox = new TextBox();
+            HttpRequestPanel.SuspendLayout();
+            HttpResponsePanel.SuspendLayout();
+            MainPanel.SuspendLayout();
+            StatusSystemPanel.SuspendLayout();
+            SystemControlPanel.SuspendLayout();
+            RequestControlPanel.SuspendLayout();
             SuspendLayout();
+            // 
+            // HttpRequestPanel
+            // 
+            HttpRequestPanel.BorderStyle = BorderStyle.FixedSingle;
+            HttpRequestPanel.Controls.Add(label5);
+            HttpRequestPanel.Controls.Add(label4);
+            HttpRequestPanel.Controls.Add(label3);
+            HttpRequestPanel.Controls.Add(label1);
+            HttpRequestPanel.Controls.Add(BodyRequestTextBox);
+            HttpRequestPanel.Controls.Add(HttpHeadersRequestTextBox);
+            HttpRequestPanel.Controls.Add(StartLineRequestTextBox);
+            HttpRequestPanel.Dock = DockStyle.Left;
+            HttpRequestPanel.Location = new Point(0, 0);
+            HttpRequestPanel.Name = "HttpRequestPanel";
+            HttpRequestPanel.Size = new Size(725, 769);
+            HttpRequestPanel.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F);
+            label5.Location = new Point(20, 395);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Body:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F);
+            label4.Location = new Point(20, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(82, 15);
+            label4.TabIndex = 8;
+            label4.Text = "HTTP headers:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F);
+            label3.Location = new Point(20, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 15);
+            label3.TabIndex = 7;
+            label3.Text = "StartLine:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 94);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Request";
+            // 
+            // BodyRequestTextBox
+            // 
+            BodyRequestTextBox.Font = new Font("Segoe UI", 9F);
+            BodyRequestTextBox.Location = new Point(20, 413);
+            BodyRequestTextBox.Multiline = true;
+            BodyRequestTextBox.Name = "BodyRequestTextBox";
+            BodyRequestTextBox.ScrollBars = ScrollBars.Both;
+            BodyRequestTextBox.Size = new Size(673, 308);
+            BodyRequestTextBox.TabIndex = 2;
+            // 
+            // HttpHeadersRequestTextBox
+            // 
+            HttpHeadersRequestTextBox.Font = new Font("Segoe UI", 9F);
+            HttpHeadersRequestTextBox.Location = new Point(20, 212);
+            HttpHeadersRequestTextBox.Multiline = true;
+            HttpHeadersRequestTextBox.Name = "HttpHeadersRequestTextBox";
+            HttpHeadersRequestTextBox.ScrollBars = ScrollBars.Both;
+            HttpHeadersRequestTextBox.Size = new Size(673, 172);
+            HttpHeadersRequestTextBox.TabIndex = 1;
+            // 
+            // StartLineRequestTextBox
+            // 
+            StartLineRequestTextBox.Font = new Font("Segoe UI", 9F);
+            StartLineRequestTextBox.Location = new Point(20, 158);
+            StartLineRequestTextBox.Name = "StartLineRequestTextBox";
+            StartLineRequestTextBox.Size = new Size(673, 23);
+            StartLineRequestTextBox.TabIndex = 0;
+            // 
+            // HttpResponsePanel
+            // 
+            HttpResponsePanel.BorderStyle = BorderStyle.FixedSingle;
+            HttpResponsePanel.Controls.Add(label8);
+            HttpResponsePanel.Controls.Add(label7);
+            HttpResponsePanel.Controls.Add(label6);
+            HttpResponsePanel.Controls.Add(label2);
+            HttpResponsePanel.Controls.Add(BodyResponseTextBox);
+            HttpResponsePanel.Controls.Add(HttpHeadersResponseTextBox);
+            HttpResponsePanel.Controls.Add(StartLineResponseTextBox);
+            HttpResponsePanel.Dock = DockStyle.Right;
+            HttpResponsePanel.Location = new Point(700, 0);
+            HttpResponsePanel.Name = "HttpResponsePanel";
+            HttpResponsePanel.Size = new Size(753, 769);
+            HttpResponsePanel.TabIndex = 0;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F);
+            label8.Location = new Point(59, 140);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 15);
+            label8.TabIndex = 12;
+            label8.Text = "StartLine:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F);
+            label7.Location = new Point(59, 194);
+            label7.Name = "label7";
+            label7.Size = new Size(82, 15);
+            label7.TabIndex = 11;
+            label7.Text = "HTTP headers:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F);
+            label6.Location = new Point(59, 395);
+            label6.Name = "label6";
+            label6.Size = new Size(37, 15);
+            label6.TabIndex = 10;
+            label6.Text = "Body:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(59, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 32);
+            label2.TabIndex = 6;
+            label2.Text = "Response";
+            // 
+            // BodyResponseTextBox
+            // 
+            BodyResponseTextBox.Font = new Font("Segoe UI", 9F);
+            BodyResponseTextBox.Location = new Point(59, 413);
+            BodyResponseTextBox.Multiline = true;
+            BodyResponseTextBox.Name = "BodyResponseTextBox";
+            BodyResponseTextBox.ScrollBars = ScrollBars.Both;
+            BodyResponseTextBox.Size = new Size(673, 308);
+            BodyResponseTextBox.TabIndex = 5;
+            // 
+            // HttpHeadersResponseTextBox
+            // 
+            HttpHeadersResponseTextBox.Font = new Font("Segoe UI", 9F);
+            HttpHeadersResponseTextBox.Location = new Point(59, 212);
+            HttpHeadersResponseTextBox.Multiline = true;
+            HttpHeadersResponseTextBox.Name = "HttpHeadersResponseTextBox";
+            HttpHeadersResponseTextBox.ScrollBars = ScrollBars.Both;
+            HttpHeadersResponseTextBox.Size = new Size(673, 172);
+            HttpHeadersResponseTextBox.TabIndex = 4;
+            // 
+            // StartLineResponseTextBox
+            // 
+            StartLineResponseTextBox.Font = new Font("Segoe UI", 9F);
+            StartLineResponseTextBox.Location = new Point(59, 158);
+            StartLineResponseTextBox.Name = "StartLineResponseTextBox";
+            StartLineResponseTextBox.Size = new Size(673, 23);
+            StartLineResponseTextBox.TabIndex = 3;
+            // 
+            // MainPanel
+            // 
+            MainPanel.BorderStyle = BorderStyle.FixedSingle;
+            MainPanel.Controls.Add(HttpRequestPanel);
+            MainPanel.Controls.Add(HttpResponsePanel);
+            MainPanel.Dock = DockStyle.Fill;
+            MainPanel.Location = new Point(0, 0);
+            MainPanel.Name = "MainPanel";
+            MainPanel.Size = new Size(1455, 771);
+            MainPanel.TabIndex = 0;
+            // 
+            // StatusSystemToolStrip
+            // 
+            StatusSystemToolStrip.Dock = DockStyle.Fill;
+            StatusSystemToolStrip.Location = new Point(0, 0);
+            StatusSystemToolStrip.Name = "StatusSystemToolStrip";
+            StatusSystemToolStrip.Size = new Size(1453, 28);
+            StatusSystemToolStrip.TabIndex = 0;
+            StatusSystemToolStrip.Text = "toolStrip1";
+            // 
+            // StatusSystemPanel
+            // 
+            StatusSystemPanel.BorderStyle = BorderStyle.FixedSingle;
+            StatusSystemPanel.Controls.Add(StatusSystemToolStrip);
+            StatusSystemPanel.Dock = DockStyle.Bottom;
+            StatusSystemPanel.Location = new Point(0, 741);
+            StatusSystemPanel.Name = "StatusSystemPanel";
+            StatusSystemPanel.Size = new Size(1455, 30);
+            StatusSystemPanel.TabIndex = 1;
+            // 
+            // SystemControlPanel
+            // 
+            SystemControlPanel.BorderStyle = BorderStyle.FixedSingle;
+            SystemControlPanel.Controls.Add(SystemControlMenuStrip);
+            SystemControlPanel.Dock = DockStyle.Top;
+            SystemControlPanel.Location = new Point(0, 0);
+            SystemControlPanel.Name = "SystemControlPanel";
+            SystemControlPanel.Size = new Size(1455, 31);
+            SystemControlPanel.TabIndex = 1;
+            // 
+            // SystemControlMenuStrip
+            // 
+            SystemControlMenuStrip.Dock = DockStyle.Fill;
+            SystemControlMenuStrip.Location = new Point(0, 0);
+            SystemControlMenuStrip.Name = "SystemControlMenuStrip";
+            SystemControlMenuStrip.Size = new Size(1453, 29);
+            SystemControlMenuStrip.TabIndex = 0;
+            SystemControlMenuStrip.Text = "menuStrip1";
+            // 
+            // RequestControlPanel
+            // 
+            RequestControlPanel.BorderStyle = BorderStyle.FixedSingle;
+            RequestControlPanel.Controls.Add(StartRequestButton);
+            RequestControlPanel.Controls.Add(UrlRequestTextBox);
+            RequestControlPanel.Dock = DockStyle.Top;
+            RequestControlPanel.Location = new Point(0, 31);
+            RequestControlPanel.Name = "RequestControlPanel";
+            RequestControlPanel.Size = new Size(1455, 38);
+            RequestControlPanel.TabIndex = 2;
+            // 
+            // StartRequestButton
+            // 
+            StartRequestButton.Font = new Font("Segoe UI", 9F);
+            StartRequestButton.Location = new Point(872, 5);
+            StartRequestButton.Name = "StartRequestButton";
+            StartRequestButton.Size = new Size(91, 23);
+            StartRequestButton.TabIndex = 2;
+            StartRequestButton.Text = "Start Request";
+            StartRequestButton.UseVisualStyleBackColor = true;
+            // 
+            // UrlRequestTextBox
+            // 
+            UrlRequestTextBox.Font = new Font("Segoe UI", 9F);
+            UrlRequestTextBox.Location = new Point(21, 5);
+            UrlRequestTextBox.Name = "UrlRequestTextBox";
+            UrlRequestTextBox.Size = new Size(845, 23);
+            UrlRequestTextBox.TabIndex = 1;
             // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1486, 960);
+            ClientSize = new Size(1455, 771);
+            Controls.Add(RequestControlPanel);
+            Controls.Add(SystemControlPanel);
+            Controls.Add(StatusSystemPanel);
+            Controls.Add(MainPanel);
             Font = new Font("Segoe UI", 18F);
-            Margin = new Padding(6, 6, 6, 6);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = SystemControlMenuStrip;
+            Margin = new Padding(6);
+            MaximizeBox = false;
             Name = "Dashboard";
             Text = "Clone Postman 1.0";
+            HttpRequestPanel.ResumeLayout(false);
+            HttpRequestPanel.PerformLayout();
+            HttpResponsePanel.ResumeLayout(false);
+            HttpResponsePanel.PerformLayout();
+            MainPanel.ResumeLayout(false);
+            StatusSystemPanel.ResumeLayout(false);
+            StatusSystemPanel.PerformLayout();
+            SystemControlPanel.ResumeLayout(false);
+            SystemControlPanel.PerformLayout();
+            RequestControlPanel.ResumeLayout(false);
+            RequestControlPanel.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel HttpRequestPanel;
+        private Panel HttpResponsePanel;
+        private Panel MainPanel;
+        private ToolStrip StatusSystemToolStrip;
+        private Panel StatusSystemPanel;
+        private TextBox StartLineRequestTextBox;
+        private TextBox HttpHeadersRequestTextBox;
+        private TextBox BodyRequestTextBox;
+        private TextBox BodyResponseTextBox;
+        private TextBox HttpHeadersResponseTextBox;
+        private TextBox StartLineResponseTextBox;
+        private Label label1;
+        private Label label2;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Panel SystemControlPanel;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private MenuStrip SystemControlMenuStrip;
+        private Panel RequestControlPanel;
+        private Button StartRequestButton;
+        private TextBox UrlRequestTextBox;
     }
 }
